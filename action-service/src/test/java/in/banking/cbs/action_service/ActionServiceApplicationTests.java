acf733af8c1d192ad31a458b2b85336f7820621e
ashar <asharkhan.eidiko@gmail.com>
2025-09-23 10:04:39 +0530

    added action service and query service

---
+++ action-service/src/test/java/in/banking/cbs/action_service/ActionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.banking.cbs.action_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
